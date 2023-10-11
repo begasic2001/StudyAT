f5eb9c1c78582aaa2b1efff4ae0b7d2cb8760cdb
begasic2001 <luongminhthanh91@gmail.com>
2023-10-11 23:28:51 +0700

    Add popover for user

--- Reactivities/src/services/activities/dashboard/ActivityListItemAttendee.tsx
+++ Reactivities/src/services/activities/dashboard/ActivityListItemAttendee.tsx
@@ -1,6 +1,7 @@
-import { List, Image } from "semantic-ui-react";
+import { List, Image, Popup } from "semantic-ui-react";
 import { Profile } from "../../../app/models/profile";
 import { Link } from "react-router-dom";
+import ProfileCard from "../../profiles/ProfileCard";
 interface Props {
   attendees: Profile[];
 }
@@ -8,9 +9,23 @@ export default function ActivityListItemAttendee({ attendees }: Props) {
   return (
     <List horizontal>
       {attendees.map((a) => (
-        <List.Item key={a.userName} as={Link} to={`/profiles/${a.userName}`}>
-          <Image size="mini" circular src={a.image || "/assets/user.png"} />
-        </List.Item>
+        <Popup
+          hoverable
+          key={a.userName}
+          trigger={
+            <List.Item
+              key={a.userName}
+              as={Link}
+              to={`/profiles/${a.userName}`}
+            >
+              <Image size="mini" circular src={a.image || "/assets/user.png"} />
+            </List.Item>
+          }
+        >
+          <Popup.Content>
+            <ProfileCard profile={a} />
+          </Popup.Content>
+        </Popup>
       ))}
     </List>
   );
