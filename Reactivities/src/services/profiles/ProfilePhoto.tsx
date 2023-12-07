f234a20f6a55581f560d7ee3beec2d2d3830809f
begasic2001 <luongminhthanh91@gmail.com>
2023-12-07 16:52:57 +0700

    getter profile user when load activities

--- Reactivities/src/services/profiles/ProfilePhoto.tsx
+++ Reactivities/src/services/profiles/ProfilePhoto.tsx
@@ -1,23 +1,46 @@
-import { Header, Tab,Card,Image } from "semantic-ui-react"
-import { Profile } from "../../app/models/profile"
+import { Header, Tab, Card, Image, Grid, Button } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
+import { useState } from "react";
 
-interface Props{
-  profile:Profile
+interface Props {
+  profile: Profile;
 }
-const ProfilePhoto = ({profile}:Props) => {
+const ProfilePhoto = ({ profile }: Props) => {
+  const {
+    profileStore: { isCurrentUser },
+  } = useStore();
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
   return (
-   <Tab.Pane>
-    <Header icon='image' content='Photo'/>
-    <Card.Group itemsPerRow={5}>
-      {profile.photos?.map(photo =>(
-          <Card key={photo.id}>
-          <Image src={photo.url} />
-        </Card>
-      ))}
-    
-    </Card.Group>
-   </Tab.Pane>
-  )
-}
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated="left" icon="image" content="Photos" />
+          {isCurrentUser && (
+            <Button
+              floated="right"
+              basic
+              content={addPhotoMode ? "Cancel" : "Add Photo"}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <p>Photo widget goes here</p>
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile.photos?.map((photo) => (
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
 
-export default ProfilePhoto
+export default ProfilePhoto;
