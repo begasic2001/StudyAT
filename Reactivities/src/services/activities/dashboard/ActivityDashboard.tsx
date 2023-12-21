55f318bc0b560721fc197c1bcce02f0c17ab7264
begasic2001 <luongminhthanh91@gmail.com>
2023-12-21 12:10:32 +0700

    Test Button Load More Activity

--- Reactivities/src/services/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/src/services/activities/dashboard/ActivityDashboard.tsx
@@ -1,23 +1,42 @@
-import { Grid } from "semantic-ui-react";
+import { Button, Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from "../../../app/layout/Loading";
 import ActivityFilter from "./ActivityFilter";
+import { PagingParams } from "../../../app/models/pagination";
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
-  const { loadActivities, activityRegistry } = activityStore;
+  const { loadActivities, activityRegistry, setPagingParams, pagination } =
+    activityStore;
+
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  const handleGetNext = () => {
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1));
+    loadActivities().then(() => setLoadingNext(false));
+  };
+
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
   }, [loadActivities]);
 
-  if (activityStore.loadingInitial)
+  if (activityStore.loadingInitial && !loadingNext)
     return <Loading content="Loading Activity..." />;
   return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList />
+        <Button
+          floated="right"
+          content="More"
+          positive
+          onClick={handleGetNext}
+          loading={loadingNext}
+          disabled={pagination?.totalPages === pagination?.currentPage}
+        />
       </Grid.Column>
       <Grid.Column width="6">
         <ActivityFilter />
